41509c90e983db50ac6d7d4e6f36f055a92d880d
subhajit-007 <subhajit.mandal@nagarro.com>
2024-07-09 23:17:23 +0530

    Update navbar component

--- bookstore-fe/src/app/app.routes.ts
+++ bookstore-fe/src/app/app.routes.ts
@@ -3,15 +3,8 @@ import { PageNotFoundComponent } from './screens/shared/page-not-found/page-not-
 import { CustomerComponent } from './screens/customer/customer.component';
 import { BookOwnerComponent } from './screens/book-owner/book-owner.component';
 
-// export const routes: Routes = [
-//   { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, // Redirect empty path to customer dashboard
-//   { path: 'dashboard', component: CustomerDashboardComponent },
-//   { path: 'book-owner/dashboard', component: OwnerDashboardComponent },
-//   // other routes go here
-//   { path: '**', component: PageNotFoundComponent }, // This should be the last route
-// ];
 export const routes: Routes = [
-  { path: '', redirectTo: 'book-owner', pathMatch: 'full' },
+  { path: '', redirectTo: 'user', pathMatch: 'full' },
   {
     path: 'user',
     component: CustomerComponent,
@@ -22,7 +15,9 @@ export const routes: Routes = [
     path: 'book-owner',
     component: BookOwnerComponent,
     loadChildren: () =>
-      import('./screens/book-owner/book-owner.route').then((m) => m.BOOK_OWNER_ROUTE),
+      import('./screens/book-owner/book-owner.route').then(
+        (m) => m.BOOK_OWNER_ROUTE
+      ),
   },
   { path: '**', component: PageNotFoundComponent }, // This should be the last route
 ];
--- bookstore-fe/src/app/components/navbar/navbar.component.ts
+++ bookstore-fe/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,26 @@
 import { Component, Input } from '@angular/core';
 import { STRINGS } from '../../configs/strings';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+
+const MatModules = [
+  MatToolbarModule,
+  MatLabel,
+  MatFormField,
+  MatInputModule,
+  MatButtonModule,
+  MatIconModule,
+];
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [...MatModules, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
@@ -14,4 +30,13 @@ export class NavbarComponent {
 
   @Input() role: string = 'admin';
 
+  // Checks user logged in or not
+  isLoggedIn(): boolean {
+    return false;
+  }
+
+  // if user is "customer" and logged in then only show cart option
+  isCartVisiable(): boolean {
+    return this.role === 'customer' && this.isLoggedIn();
+  }
 }
