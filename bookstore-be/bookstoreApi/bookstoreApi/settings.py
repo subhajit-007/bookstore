f6f828d2fb079f55b47919b95ca951f1513a2838
subhajit-007 <subhajit.mandal@nagarro.com>
2024-07-19 13:00:41 +0530

    Update

--- bookstore-be/bookstoreApi/bookstoreApi/settings.py
+++ bookstore-be/bookstoreApi/bookstoreApi/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-59r1@oor^oax%0i#3x+&wno)14t*$kx0knuttl56(udjyti8r6'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
